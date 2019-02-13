d7db3066d7e07b6ecc0e6344be3a36699973c857
AliCinder <43942992+XiaoyuRenn@users.noreply.github.com>
2019-02-13 18:18:29 +0800

    Update 1001.cpp

--- PAT BASIC LEVEL/1001.cpp
+++ PAT BASIC LEVEL/1001.cpp
@@ -1,1 +1,23 @@
 
+// 1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n, sum = 0;
+	cin >> n;
+	while (n != 1) {
+		if (n % 2 == 0) {
+			n = n / 2;
+			sum++;
+		}
+		else {
+			n = (3 * n + 1) / 2;
+				sum++;
+		}
+	}
+	cout << sum << endl;
+	return 0;
+}
